36c301d58e62435410fd14b99fd8a76ea4eb1b52
500229522 <balangodamuhandir@loyalistcollege.com>
2023-11-10 15:14:57 -0500

    [PR] Minor fixes

--- buyer/topBarNav.php
+++ buyer/topBarNav.php
@@ -15,11 +15,8 @@
       <nav class="main-header navbar navbar-expand navbar-light shadow text-sm">
         <!-- Left navbar links -->
         <ul class="navbar-nav">
-          <li class="nav-item">
-          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-          </li>
           <li class="nav-item d-none d-sm-inline-block">
-            <a href="<?php echo base_url ?>" class="nav-link"><?php echo (!isMobileDevice()) ? $_settings->info('name'):$_settings->info('short_name'); ?> - Buyer Portal</a>
+            <a class="nav-link"><?php echo (!isMobileDevice()) ? $_settings->info('name'):$_settings->info('short_name'); ?> - Buyer Portal</a>
           </li>
         </ul>
         <!-- Right navbar links -->
