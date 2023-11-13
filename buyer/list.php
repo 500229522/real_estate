a073a3f8bd9f98122730dbfe31a2a9bb67c46f32
500229522 <balangodamuhandir@loyalistcollege.com>
2023-11-13 16:17:43 -0500

    [PR] Add pagination and search in property list for buyer

--- buyer/list.php
+++ buyer/list.php
@@ -68,3 +68,40 @@
 		</div>
 	</div>
 </div>
+<script>
+	$(document).ready(function(){
+		$('.delete').click(function(){
+			_conf("Are you sure you want to delete this property permanently?","delete_property",[$(this).attr('data-id')])
+		})
+		$('.table').dataTable({
+			columnDefs: [
+					{ orderable: false, targets: [5] }
+			],
+			order:[0,'asc']
+		});
+		$('.dataTable td,.dataTable th').addClass('py-1 px-2 align-middle')
+	})
+
+	function delete_property($id){
+		start_loader();
+		$.ajax({
+			url:_base_url_+"classes/property.php?f=delete",
+			method:"POST",
+			data:{id: $id},
+			dataType:"json",
+			error:err=>{
+				console.log(err)
+				alert_toast("An error occured.",'error');
+				end_loader();
+			},
+			success:function(resp){
+				if(typeof resp== 'object' && resp.status == 'success'){
+					location.replace('./?page=property_list');
+				}else{
+					alert_toast("An error occured.",'error');
+					end_loader();
+				}
+			}
+		})
+	}
+</script>
