d226242893ccd9d5f6bf1c5b40f32d5e0ef1708d
500229522 <balangodamuhandir@loyalistcollege.com>
2023-11-12 23:27:25 -0500

    [PR] Add more comments

--- utils/sess_auth.php
+++ utils/sess_auth.php
@@ -1,5 +1,6 @@
 <?php 
 if (session_status() == PHP_SESSION_NONE) {
+    // Start session
     session_start();
 }
 if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') 
@@ -9,17 +10,18 @@ else
 $link .= "://"; 
 $link .= $_SERVER['HTTP_HOST']; 
 $link .= $_SERVER['REQUEST_URI'];
+
+// If userdata session vaariable is not set, redirect to login page
 if(!isset($_SESSION['userdata']) && !strpos($link, 'login.php') && !strpos($link, 'register.php')){
 	redirect('login.php');
 }
+
+// If userdata session variable is set and the role is Buyer, redirect to buyer portal
 if(isset($_SESSION['userdata']) && (strpos($link, 'login.php') || strpos($link, 'register.php')) && $_SESSION['userdata']['role'] ==  'Buyer'){
 	redirect('buyer/index.php');
 }
+
+// If userdata session variable is set and the role is Agent, redirect to agent portal
 if(isset($_SESSION['userdata']) && (strpos($link, 'login.php') || strpos($link, 'register.php')) && $_SESSION['userdata']['role'] ==  'Agent'){
 	redirect('agent/index.php');
 }
-$module = array('Buyer','Agent');
-// if(isset($_SESSION['userdata']) && (strpos($link, 'index.php')) && $_SESSION['userdata']['role'] !=  ''){
-// 	echo "<script>alert('Access Denied!');location.replace('".base_url.$module[$_SESSION['userdata']['login_type']]."');</script>";
-//     exit;
-// }
