12194d81b6273966a3738f3ede9d35f3cc3d2e3a
500229522 <balangodamuhandir@loyalistcollege.com>
2023-11-21 10:22:12 -0500

    [PR] Add seed data

--- database/real_estate_db.sql
+++ database/real_estate_db.sql
@@ -44,6 +44,14 @@ CREATE TABLE `property_types` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
+INSERT INTO `property_types`(`id`,`type`,`description`,`created_date`,`updated_date`,`deleted_date`) VALUES (1,'Town House','','2023-11-20 00:00:00',NULL,NULL);
+
+INSERT INTO `property_types`(`id`,`type`,`description`,`created_date`,`updated_date`,`deleted_date`) VALUES (2,'Condo','','2023-11-20 00:00:00',NULL,NULL);
+
+INSERT INTO `property_types`(`id`,`type`,`description`,`created_date`,`updated_date`,`deleted_date`) VALUES (3,'Commercial','','2023-11-20 00:00:00',NULL,NULL);
+
+INSERT INTO `property_types`(`id`,`type`,`description`,`created_date`,`updated_date`,`deleted_date`) VALUES (4,'Individual Villa','','2023-11-20 00:00:00',NULL,NULL);
+
 
 CREATE TABLE `amenities` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -54,3 +62,17 @@ CREATE TABLE `amenities` (
   `deleted_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `amenities`(`id`,`amenity`,`description`,`created_date`,`updated_date`,`deleted_date`) VALUES (1,'Swimming Pool','','2023-11-20 00:00:00',NULL,NULL);
+
+INSERT INTO `amenities`(`id`,`amenity`,`description`,`created_date`,`updated_date`,`deleted_date`) VALUES (2,'Free Wi-fi','','2023-11-20 00:00:00',NULL,NULL);
+
+INSERT INTO `amenities`(`id`,`amenity`,`description`,`created_date`,`updated_date`,`deleted_date`) VALUES (3,'Balcony','','2023-11-20 00:00:00',NULL,NULL);
+
+INSERT INTO `amenities`(`id`,`amenity`,`description`,`created_date`,`updated_date`,`deleted_date`) VALUES (4,'Garden','','2023-11-20 00:00:00',NULL,NULL);
+
+INSERT INTO `amenities`(`id`,`amenity`,`description`,`created_date`,`updated_date`,`deleted_date`) VALUES (5,'Furnished','','2023-11-20 00:00:00',NULL,NULL);
+
+INSERT INTO `amenities`(`id`,`amenity`,`description`,`created_date`,`updated_date`,`deleted_date`) VALUES (6,'Parking','','2023-11-20 00:00:00',NULL,NULL);
+
+INSERT INTO `amenities`(`id`,`amenity`,`description`,`created_date`,`updated_date`,`deleted_date`) VALUES (7,'Gym','','2023-11-20 00:00:00',NULL,NULL);
